69eb4b98cccc99fb276b151e3b022764e1c0e630
Abhiraj Mandal <82170381+NyteZoid@users.noreply.github.com>
2025-07-29 01:28:45 +0530

    Update studentdatabase.py

--- pythonmysqlinterface/studentdatabase.py
+++ pythonmysqlinterface/studentdatabase.py
@@ -24,57 +24,61 @@ def show():
         print(row)
 
 def insert():
-    a = int(input("Do you wish to input any new records? Press 1 for yes, 2 for no: "))
     def newrec():
-        if a == 1:
-            sql = "INSERT INTO STUDENT (id,name,stream,marks) VALUES (%s,%s,%s,%s)"
-            i = int(input("Enter student id: "))
-            nm = input("Enter student name: ")
-            st = input("Enter stream: ")
-            mk = int(input("Enter marks: "))
-            val = (i,nm,st,mk)
-            mycursor.execute(sql,val)
-            myconn.commit()
-            show()
-            b = int(input("Do you wish to input any more records? Press 1 for yes, 2 for no: "))
-            if b == 1:
-                newrec()
+        sql = "INSERT INTO STUDENT (id,name,stream,marks) VALUES (%s,%s,%s,%s)"
+        i = int(input("Enter student id: "))
+        nm = input("Enter student name: ")
+        st = input("Enter stream: ")
+        mk = int(input("Enter marks: "))
+        val = (i,nm,st,mk)
+        mycursor.execute(sql,val)
+        myconn.commit()
+        show()
+        b = int(input("Do you wish to input any more records? Press 1 for yes, 2 for no: "))
+        if b == 1:
+            newrec()
     newrec()
 
 def delete():
-    a = int(input("Do you wish to delete any existing records? Press 1 for yes, 2 for no: "))
     def delrec():
-        if a == 1:
-            d = int(input("Enter id of the student whose record you wish to delete: "))
-            sql = "DELETE FROM STUDENT WHERE id = %s"
-            val = (d,)
-            mycursor.execute(sql,val)
-            myconn.commit()
-            show()
-            b = int(input("Do you wish to delete any more records? Press 1 for yes, 2 for no: "))
-            if b == 1:
-                delrec()
+        d = int(input("Enter id of the student whose record you wish to delete: "))
+        sql = "DELETE FROM STUDENT WHERE id = %s"
+        val = (d,)
+        mycursor.execute(sql,val)
+        myconn.commit()
+        show()
+        b = int(input("Do you wish to delete any more records? Press 1 for yes, 2 for no: "))
+        if b == 1:
+            delrec()
     delrec()
 
 def update():
-    a = int(input("Do you wish to update any existing records? Press 1 for yes, 2 for no: "))
     def updrec():
-        if a == 1:
-            o = int(input("Enter id of the student whose marks you wish to update: "))
-            n = input("Enter updated marks: ")
-            sql = "UPDATE student SET  marks = %s WHERE id = %s"
-            val = (n,o)
-            mycursor.execute(sql,val)
-            myconn.commit()
-            show()
-            b = int(input("Do you wish to update any more records? Press 1 for yes, 2 for no: "))
-            if b == 1:
-                updrec()
+        o = int(input("Enter id of the student whose marks you wish to update: "))
+        n = input("Enter updated marks: ")
+        sql = "UPDATE student SET  marks = %s WHERE id = %s"
+        val = (n,o)
+        mycursor.execute(sql,val)
+        myconn.commit()
+        show()
+        b = int(input("Do you wish to update any more records? Press 1 for yes, 2 for no: "))
+        if b == 1:
+            updrec()
     updrec()
 
 show()
-insert()
-delete()
-update()
+
+while True:
+    m = int(input("Press 1 to insert any new records\nPress 2 to delete any existing records\nPress 3 to update any existing records\nPress 4 to exit: "))
+    if m == 1:
+        insert()
+    elif m == 2:
+        delete()
+    elif m == 3:
+        update()
+    elif m == 4:
+        break
+    else:
+        print("Invalid choice")
 
 #end
