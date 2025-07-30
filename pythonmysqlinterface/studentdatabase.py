71e4bbd709eb6590877fbca8e45a508a1d8eeb16
Abhiraj Mandal <82170381+NyteZoid@users.noreply.github.com>
2025-07-31 01:39:02 +0530

    Update studentdatabase.py

--- pythonmysqlinterface/studentdatabase.py
+++ pythonmysqlinterface/studentdatabase.py
@@ -15,7 +15,7 @@ else:
 
 mycursor = myconn.cursor()
 
-def show():
+def showTab():
     mycursor.execute("SELECT * FROM STUDENT")
     data = mycursor.fetchall()
     count = mycursor.rowcount
@@ -23,8 +23,8 @@ def show():
     for row in data:
         print(row)
 
-def insert():
-    def newrec():
+def insTab():
+    def newRec():
         sql = "INSERT INTO STUDENT (id,name,stream,marks) VALUES (%s,%s,%s,%s)"
         i = int(input("Enter student id: "))
         nm = input("Enter student name: ")
@@ -33,50 +33,66 @@ def insert():
         val = (i,nm,st,mk)
         mycursor.execute(sql,val)
         myconn.commit()
-        show()
+        showTab()
         b = int(input("Do you wish to input any more records? Press 1 for yes, 2 for no: "))
         if b == 1:
-            newrec()
-    newrec()
+            newRec()
+    newRec()
 
-def delete():
-    def delrec():
+def delTab():
+    def delRec():
         d = int(input("Enter id of the student whose record you wish to delete: "))
         sql = "DELETE FROM STUDENT WHERE id = %s"
         val = (d,)
         mycursor.execute(sql,val)
         myconn.commit()
-        show()
+        showTab()
         b = int(input("Do you wish to delete any more records? Press 1 for yes, 2 for no: "))
         if b == 1:
-            delrec()
-    delrec()
+            delRec()
+    delRec()
 
-def update():
-    def updrec():
+def updTab():
+    def updRec():
         o = int(input("Enter id of the student whose marks you wish to update: "))
-        n = input("Enter updated marks: ")
-        sql = "UPDATE student SET  marks = %s WHERE id = %s"
+        def updSpec():
+            global s
+            g = int(input("Which field do you wish to change?\nPress 1 for id\nPress 2 for name\nPress 3 for stream\nPress 4 for marks: "))
+            if g == 1:
+                s = "id"
+            elif g == 2:
+                s = "name"
+            elif g == 3:
+                s = "stream"
+            elif g == 4:
+                s = "marks"
+            else:
+                print("Invalid choice")
+                updSpec()
+        updSpec()
+        n = input("Enter updated value: ")
+        sql = f"UPDATE student SET {s} = %s WHERE id = %s"
         val = (n,o)
         mycursor.execute(sql,val)
         myconn.commit()
-        show()
+        showTab()
         b = int(input("Do you wish to update any more records? Press 1 for yes, 2 for no: "))
         if b == 1:
-            updrec()
-    updrec()
+            updRec()
+    updRec()
 
-show()
+showTab()
 
 while True:
     m = int(input("Press 1 to insert any new records\nPress 2 to delete any existing records\nPress 3 to update any existing records\nPress 4 to exit: "))
     if m == 1:
-        insert()
+        insTab()
     elif m == 2:
-        delete()
+        delTab()
     elif m == 3:
-        update()
+        updTab()
     elif m == 4:
+        print("Thank you for using this app!")
         break
     else:
         print("Invalid choice")
